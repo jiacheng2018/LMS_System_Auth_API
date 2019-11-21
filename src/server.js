3230a0baf65f2202784d223378d8b592823ce086
jiacheng2018 <penggysosmart@gmail.com>
2019-11-22 08:31:57 +1000

    Commit
    Build up scheme

--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
+// This is server
+//  client ID 214420770396-tsk489qn2kc6nteuskp6eksgh3dgpc23.apps.googleusercontent.com
+//  client secret xvpvbz9MXXyQ6bzvtNQj1zXO
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Using google Strategy
+const keys = require('./Config/Prod');
 const app = express();
+passport.use(
+          new GoogleStrategy(
+                    {
+                              clientID: process.env.googleClientID, //global variable
+                              clientSecret: process.env.googleClientSecret,
+                              callbackURL: '/auth/google/callback'
+                    },
+                    accesstoken => {
+                              console.log(accesstoken);
+                    }
+          )
+); //register service
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const morganLog =
           process.env.NODE_ENV === 'production'
                     ? morgan('common')
@@ -15,6 +33,12 @@ app.use(morganLog);
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
+app.use(
+          '/auth/google',
+          passport.authenticate('google', {
+                    scope: ['profile', 'email'] //user profile
+          })
+);
 app.get('/', (req, res) => {
           res.json('hello');
 });
